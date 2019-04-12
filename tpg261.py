98a78a5e43d42fb32b83095f1ea30b6770a069ea
masui <masui@ogawalabnoMacBook-Pro.local>
2019-04-12 15:21:11 +0900

    add

--- tpg261.py
+++ tpg261.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python3
 
-import sys, time, pymeasure, rospy, std_msgs
+import sys, time, pymeasure, rospy, std_msgs, serial
 
 
 class device(object):
 
+    def __init__(self):
+    self.tpg261 = serial.Serial("/dev/ttyUSB1",timeout=1)
+
+
     def pressure_device(self):
-         tpg.tpg261.write(b"PR1 \r\n")
+         self.tpg261.write(b"PR1 \r\n")
          time.sleep(0.3)
-         tpg261_driver.tpg261.write(b"\x05")
+         self..tpg261.write(b"\x05")
          time.sleep(0.3)
          raw = tpg261_driver.tpg261.readline()
          status = raw[0:1]
          pressure = str(raw[2:13])
 
     def pressure_both(self):
-         tpg261_driver.tpg261.write(b"PRX \r\n")
+         self.tpg261.write(b"PRX \r\n")
          time.sleep(0.3)
-         tpg261_driver.tpg261.write(b"\x05")
+         self.tpg261.write(b"\x05")
          time.sleep(0.3)
          raw = tpg261_driver.tpg261.readline()
          status1 = raw[0:1]
@@ -25,43 +29,43 @@ class device(object):
          pressure12 = str(raw[2:13],raw[16:27])
 
     def gauge_check(self,gauge1 = 0,gauge2 = 0):
-        tpg261_driver.tpg261.write(b"SEN , gague1 , gague2 \r\n")
+        self.tpg261.write(b"SEN , gague1 , gague2 \r\n")
         time.sleep(0.3)
-        tpg261_driver.tpg261.write(b"\x05")
+        self.tpg261.write(b"\x05")
         time.sleep(0.3)
         get = tpg261_driver.tpg261.readline()
         status1 = str(get[0:1])
         status2 = str(get[2:3])
 
     def gauge_change(self,gague1,gague2):
-        tpg261_driver.tpg261.write(b"SEN , gague1 , gague2 \r\n")
+        self.tpg261.write(b"SEN , gague1 , gague2 \r\n")
         time.sleep(0.3)
-        tpg261_driver.tpg261.write(b"\x05")
+        self.tpg261.write(b"\x05")
         time.sleep(0.3)
         get = tpg261_driver.tpg261.readline()
         status1 = str(get[0:1])
         status2 = str(get[2:3])
 
     def change_gague1(self):
-        tpg261_driver.tpg261.write(b"SCT , 0\r\n")
+        self.tpg261.write(b"SCT , 0\r\n")
         time.sleep(0.3)
-        tpg261_driver.tpg261.write(b"\x05")
+        self..tpg261.write(b"\x05")
         time.sleep(0.3)
         get = tpg261_driver.tpg261.readline()
         status = str(get[0:1])
 
     def change_gague2(self):
-        tpg261_driver.tpg261.write(b"SCT , 1\r\n")
+        self.tpg261.write(b"SCT , 1\r\n")
         time.sleep(0.3)
-        tpg261_driver.tpg261.write(b"\x05")
+        self.tpg261.write(b"\x05")
         time.sleep(0.3)
         get = tpg261_driver.tpg261.readline()
         status = str(get[0:1])
 
     def query_error(self):
-        tpg261_driver.tpg261.write(b"ERR \r\n")
+        self.tpg261.write(b"ERR \r\n")
         time.sleep(0.3)
-        tpg261_driver.tpg261.write(b"\x05")
+        self.tpg261.write(b"\x05")
         time.sleep(0.3)
         get = tpg261_driver.tpg261.readline()
         status = str(get[0:4])
