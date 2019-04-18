0118b102e4c8b5ea3d6ca8ec84186d1501351114
masui <masui@ogawalabnoMacBook-Pro.local>
2019-04-18 14:48:06 +0900

    add

--- tpg261.py
+++ tpg261.py
@@ -16,7 +16,7 @@ class device(object):
          time.sleep(0.3)
          self.raw = self.tpg261.readline()
          pressure = self.raw[2:13]
-         pressure = decode.pressure()
+         pressure = pressure.hex()
          return pressure
 
     def pressure_error(self):
